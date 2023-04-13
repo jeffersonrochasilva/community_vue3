23741b21a884ed76299cdef3d1229798e339f6b2
DaviAugustoMS     <jeffersonrochadasilva667@gmail.com>
2023-04-13 18:08:19 -0300

    community vue3

---
+++ src/components/Card/index.vue
@@ -0,0 +1,117 @@
+<template>
+  <div
+    class="card"
+    :style="{
+      '--background': props.corFundo,
+    }"
+  >
+    <p class="card__subtitle">Para você começar</p>
+    <div class="card__titles">
+      <span
+        :style="{ '--colorTextTitle': props.colorTextTitle }"
+        class="card__titles__title"
+        >{{ props.textTitle1 }}</span
+      >
+      <span
+        :style="{ '--colorTextTitle': props.colorTextTitle }"
+        class="card__titles__title"
+        >R$ {{ subTextTitle }} /mês</span
+      >
+    </div>
+    <button
+      :style="{
+        '--colorFundoButton': props.corFundoButton,
+        '--colorText': props.corButtonWrite,
+      }"
+    >
+      Assinar agora
+    </button>
+    <v-divider />
+    <div style="height: 40%">
+      <div class="card__description" v-for="(item, i) in items" :key="i">
+        <div class="card__description__camp">
+          <v-icon color="green">mdi-check</v-icon>
+          <p
+            :style="{ '--colorText': props.colorText }"
+            class="card__description__camp__text"
+          >
+            {{ item }}
+          </p>
+        </div>
+        <v-icon :style="{ color: props.colorText }"
+          >mdi-minus-circle-outline</v-icon
+        >
+      </div>
+    </div>
+  </div>
+</template>
+<script lang="ts" setup>
+const props = defineProps<{
+  corFundo: String;
+  corFundoButton: String;
+  corButtonWrite: String;
+  textTitle1: String;
+  subTextTitle: String;
+  colorTextTitle: String;
+  colorText: String;
+  items: String[];
+}>();
+</script>
+<style lang="scss" scoped>
+p,
+span {
+  font-family: sans-serif;
+}
+.card {
+  --background: red;
+  width: 384px;
+  height: 454px;
+  border-radius: 5px;
+  background: var(--background, blue);
+  margin: 32px;
+  padding: 50px 32px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  &__subtitle {
+    color: #996dff;
+    font-size: 18px;
+    font-weight: bold;
+  }
+  &__titles {
+    display: flex;
+    justify-content: space-between;
+    span {
+      --colorTextTitle: #000;
+      font-size: 25px;
+      font-weight: bold;
+      color: var(--colorTextTitle);
+    }
+  }
+  button {
+    --colorFundoButton: #303030;
+    --borderColor: #303030;
+    --colorText: #303030;
+    width: 80%;
+    height: 45px;
+    background: var(--colorFundoButton, red);
+    border-radius: 4px;
+    border: 1px solid var(--borderColor, red);
+    color: var(--colorText, red);
+    margin-left: 10%;
+  }
+  &__description {
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 15px;
+    &__camp {
+      display: flex;
+
+      &__text {
+        --colorText: red;
+        color: var(--colorText);
+      }
+    }
+  }
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="home">
+    <Card
+      :corFundo="card"
+      :corFundoButton="corFundoButtonWrite"
+      :corButtonWrite="corButtonNoWrite"
+      :textTitle1="textTitle1"
+      :subTextTitle="subTextTitle1"
+      :colorTextTitle="colorTextTitle1"
+      colorText="#000"
+      :items="itemsOne"
+    />
+    <Card
+      :corFundo="cardCenter"
+      :corFundoButton="corFundoButtonNoWrite"
+      :corButtonWrite="corButtonWrite"
+      :textTitle1="textTitle2"
+      :subTextTitle="subTextTitle2"
+      :colorTextTitle="colorTextTitle2"
+      colorText="#cdcdcd"
+      :items="itemsTwo"
+    />
+    <Card
+      :corFundo="card"
+      :corFundoButton="corFundoButtonWrite"
+      :corButtonWrite="corButtonNoWrite"
+      :textTitle1="textTitle3"
+      :subTextTitle="subTextTitle3"
+      :colorTextTitle="colorTextTitle1"
+      colorText="#000"
+      :items="itemsThree"
+    />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import Card from "@/components/Card/index.vue";
+import { Ref, ref } from "vue";
+const card: Ref<string> = ref("#FFFFFF");
+const cardCenter: Ref<string> = ref("#0F172A");
+const corFundoButtonWrite: Ref<string> = ref("#FFF");
+const corFundoButtonNoWrite: Ref<string> = ref("#996DFF");
+const corButtonWrite: Ref<string> = ref("#fff");
+const corButtonNoWrite: Ref<string> = ref("#0F172A");
+const textTitle1: Ref<string> = ref("Essentials");
+const textTitle2: Ref<string> = ref("Ultimate");
+const textTitle3: Ref<string> = ref("Interprise");
+const subTextTitle1: Ref<string> = ref("19,97");
+const subTextTitle2: Ref<string> = ref("29,97");
+const subTextTitle3: Ref<string> = ref("");
+const colorTextTitle1: Ref<string> = ref("#000");
+const colorTextTitle2: Ref<string> = ref("#fff");
+const itemsOne: Ref<string[]> = ref(["Até 3 usuários", "Autoatendimento"]);
+const itemsTwo: Ref<string[]> = ref([
+  "Usuários ilimitados",
+  "Suporte 24/7",
+  "CSM Dedicado",
+  "Treinamentos",
+]);
+const itemsThree: Ref<string[]> = ref([
+  "Plano customizado especialmente para a necessidade de seu negocio",
+]);
+</script>
+<style lang="scss" scoped>
+.home {
+  width: 100%;
+  height: 100vh;
+  background: #f8fafc;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+
+  h1 {
+    color: red;
+  }
+}
+</style>
